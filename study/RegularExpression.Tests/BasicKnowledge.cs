3cf3af87e299ab48505dbe148367f3c06b38358a
xiaochao <1036717543@qq.com>
2019-08-27 23:28:56 +0800

    示例完善

--- study/RegularExpression.Tests/BasicKnowledge.cs
+++ study/RegularExpression.Tests/BasicKnowledge.cs
@@ -342,5 +342,22 @@ namespace RegularExpression.Tests
             Assert.DoesNotMatch(pattern, "wwwxbaiduycom.cn");
             Assert.DoesNotMatch(pattern, "www baidu com.cn");
         }
+
+        /// <summary>
+        /// 获取引号内得字符串 注意下,比较难理解
+        /// </summary>
+        [Fact]
+        public void GetQuotationMark()
+        {
+            //C#字符串中引号需要转义 两个引号之间不是引号的且中间可以出现多个字符的字符串
+            var pattern = "\"[^\"]*\"";
+            var content = "<script src=\"1.html\"></script>";
+            var matchs = Regex.Match(content, pattern);
+            Assert.Equal("1.html", matchs.Value.Replace("\"", ""));
+        }
     }
 }
+
+
+
+

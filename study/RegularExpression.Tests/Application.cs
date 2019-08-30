b278f4bc7900e8f347a1780d4c041066fc73d2b2
郑超 <1036717543@qq.com>
2019-08-30 10:32:21 +0800

    示例修正

--- study/RegularExpression.Tests/Application.cs
+++ study/RegularExpression.Tests/Application.cs
@@ -11,12 +11,16 @@ namespace RegularExpression.Tests
     public class Application
     {
         /// <summary>
-        /// 快速抽取script标签中的url
+        /// 获取引号内得字符串 注意下,比较难理解
         /// </summary>
         [Fact]
-        public void GetScriptUrl()
+        public void GetQuotationMark()
         {
-            
+            //C#字符串中引号需要转义 两个引号之间不是引号的且中间可以出现多个字符的字符串
+            var pattern = "\"[^\"]*\"";
+            var content = "<script src=\"1.html\"></script>";
+            var matchs = Regex.Match(content, pattern);
+            Assert.Equal("1.html", matchs.Value.Replace("\"", ""));
         }
     }
 }
